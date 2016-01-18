0c93d4e4dc1fe1f029734c4132075b60dfe2fd82
Romeo Van Snick <romeovs@gmail.com>
2016-01-18 18:55:04 +0100

    fix and validate email header

--- src/lib/read.js
+++ src/lib/read.js
@@ -15,6 +15,54 @@ const read = function (filename) {
   });
 };
 
+const recipients = function (data) {
+  if ( data && data.join ) {
+     return data.join(', ');
+  } else {
+    return data;
+  }
+};
+
+const req = function (obj, ...names) {
+  const [name, ...rest] = names;
+
+  if ( rest.length === 0 ) {
+    if ( obj[name] === undefined ) {
+      throw new Error(`${name} is required!`);
+    }
+  } else {
+    req(obj, name);
+    req(obj, ...rest);
+  }
+};
+
+const strict = function (obj, valid) {
+  Object.keys(obj).forEach(function (key) {
+    if ( valid.indexOf(key) < 0 ) {
+      console.log(`Warning: unused option in header \`${key}\``);
+    }
+  });
+};
+
+// fixes attributes
+const fix = function (attrs) {
+
+  req(attrs,
+      'from'
+    , 'to'
+    , 'subject'
+    );
+
+  strict(attrs, ['from', 'to', 'subject', 'cc', 'bcc']);
+
+  return {
+    ...attrs
+  , to:   recipients(attrs.to)
+  , cc:   recipients(attrs.cc)
+  , bcc:  recipients(attrs.bcc)
+  };
+};
+
 export default async function (filename) {
   const contents = await read(filename);
   const replaced = await replace(process.env, contents)
@@ -25,7 +73,7 @@ export default async function (filename) {
   } = frontmatter(replaced);
 
   return {
-    ...attributes
+    ...fix(attributes)
   , markdown: body
   , text: text(body)
   };
