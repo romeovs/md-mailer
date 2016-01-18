d000b2171664ca245cd395a3e5d81c7bac6a44ff
Romeo Van Snick <romeovs@gmail.com>
2016-01-18 15:42:11 +0100

    move config code to separate folder

--- src/index.js
+++ src/index.js
@@ -1,104 +1,16 @@
 #! /usr/bin/env node
-import yargs from 'yargs'
-import mail  from './lib/mail'
-import read  from './lib/read'
-import error from './lib/error'
+import mail   from './lib/mail'
+import read   from './lib/read'
+import error  from './lib/error'
+import config from './lib/config'
 
-// environment variable name to use for password
-const USER = 'EMAILER_USER';
-const PASS = 'EMAILER_PASSWORD';
-
-const argv =
-  yargs
-
-    // ask for host
-    .describe('h', 'Email provider host.')
-    .demand('host')
-    .nargs('host', 1)
-    .alias('h', 'host')
-
-    // ask for user
-    .describe('u', 'Email provider username.')
-    .demand('user')
-    .nargs('user', 1)
-    .alias('u', 'user')
-    .default('u', process.env[USER])
-
-    // check for pass
-    .describe('p', `Email provider password. (This might be unsafe, consider using the environment variable $${PASS}).`)
-    .nargs('pass', 1)
-    // .default('pass', process.env[PASS])
-    .alias('p', 'pass')
-    .alias('p', 'password')
-
-    // check for port
-    .describe('P', 'Port to be used.')
-    .alias('P', 'port')
-    .nargs('port', 1)
-
-    // wether or not to use ssl
-    .describe('s', 'Enable ssl.')
-    .boolean('ssl')
-    .alias('s', 'ssl')
-
-    // enable help
-    .help('?')
-    .alias('?', 'help')
-
-    // set dry-run option
-    .describe('d', 'dry-run. Doesn\'t send any emails')
-    .alias('d', 'dry-run')
-    .boolean('d')
-
-    .demand(1)
-    .argv
-    ;
-
-const validate = async function (options) {
-  if ( !options.user ) {
-    throw new Error(`Missing required argument: user\nset it trough the --user option or via $${USER}.`);
-  }
-
-  let pass;
-  if ( !options.pass ) {
-    // lookup pass in env
-    if (process.env[PASS] ) {
-      pass = process.env[PASS];
-    } else {
-      throw new Error(`Missing required argument: pass\nset it trough the --pass option or via $${PASS}.`);
-    }
-  } else {
-    console.log(`Warning: using the pass command line argument may be dangerous, consider using the $${PASS} enviroment variable.`);
-    pass = options.pass;
-  }
-
-  return {
-    host:   options.host
-  , secure: options.ssl
-  , port :  options.port
-  , auth: {
-      user: options.user
-    , pass
-    }
-  , files: options._
-  };
-};
-
-validate(argv)
+config()
   .then(async function (options) {
     const { files } = options;
     return [options, await Promise.all(files.map(read))];
   })
   .then(async function ([options, messages]) {
-    const sent = messages.map(function (message) {
-      if ( options.dryRun === true ) {
-        console.log(message);
-        return message;
-      } else {
-        return mail(options, message);
-      }
-    });
-
+    const sent = messages.map(msg => mail(options, msg))
     return Promise.all(sent);
   })
   .then(function (sent) {
---
+++ src/lib/config.js
@@ -0,0 +1,75 @@
+import yargs from 'yargs'
+
+// environment variable name to use for password
+const prefix = 'MDMAILER_';
+const USER = `${prefix}USER`;
+const PASS = `${prefix}PASS`;
+
+const validate = async function (options) {
+  return {
+    host:   options.host
+  , secure: options.ssl
+  , port :  options.port
+  , auth: {
+      user: options.user
+    , pass: options.pass
+    }
+  , files: options._
+  , dryRun: options.dryRun
+  };
+};
+
+
+export default function () {
+  const argv =
+    yargs
+      .env(prefix)
+      .showHelpOnFail(false, 'Specify --help for available options')
+      .strict()
+
+      // ask for host
+      .describe('h', 'Email provider host.')
+      .demand('host')
+      .nargs('host', 1)
+      .alias('h', 'host')
+
+      // ask for user
+      .describe('u', 'Email provider username.')
+      .demand('user')
+      .nargs('user', 1)
+      .alias('u', 'user')
+      .default('u', process.env[USER])
+
+      // check for pass
+      .describe('p', `Email provider password. (This might be unsafe, consider using the environment variable $${PASS}).`)
+      .demand('pass')
+      .nargs('pass', 1)
+      .alias('p', 'pass')
+      .alias('p', 'password')
+
+      // check for port
+      .describe('P', 'Port to be used.')
+      .alias('P', 'port')
+      .nargs('port', 1)
+
+      // wether or not to use ssl
+      .describe('s', 'Enable ssl.')
+      .boolean('ssl')
+      .alias('s', 'ssl')
+
+      // enable help
+      .help('?')
+      .alias('?', 'help')
+
+      // set dry-run option
+      .describe('d', 'dry-run. Doesn\'t send any emails')
+      .alias('d', 'dry-run')
+      .boolean('d')
+
+      .demand(1)
+      .argv
+      ;
+
+  return validate(argv);
+};
+
